fc02ff75130457a67947ce3bb775e3e947edc7e4
Frame1910 <dpmeiring@gmail.com>
2020-10-24 17:28:19 +0800

    DevController can now set roles for users

--- Controllers/DevController.cs
+++ Controllers/DevController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.WebSockets;
 using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
@@ -12,11 +13,12 @@ namespace SportsManagementSystem.Controllers
     public class DevController : Controller
     {
         private readonly RoleManager<IdentityRole> roleManager;
-        private readonly UserManager<ApplicationUser> userManager;
+        private readonly UserManager<IdentityUser> userManager;
 
-        public DevController(RoleManager<IdentityRole> roleManager)
+        public DevController(RoleManager<IdentityRole> roleManager, UserManager<IdentityUser> userManager)
         {
             this.roleManager = roleManager;
+            this.userManager = userManager;
         }
         
         [HttpGet]
@@ -41,6 +43,22 @@ namespace SportsManagementSystem.Controllers
                     return RedirectToAction("Index", "Home");
                 }
             }
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult SetUserRole()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SetUserRole(SetUserRoleModel model)
+        {
+            var user = await userManager.FindByNameAsync(model.UserName);
+
+            var result = await userManager.AddToRoleAsync(user, model.RoleName);
 
             return View();
         }
---
+++ Models/SetUserRoleModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsManagementSystem.Models
+{
+    public class SetUserRoleModel
+    {
+        public string UserName { get; set; }
+        public string RoleName { get; set; }
+    }
+}

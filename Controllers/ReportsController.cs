9587843a21a872936b49f15b45f11130357d2738
Frame1910 <dpmeiring@gmail.com>
2020-10-24 23:05:03 +0800

    Misc Styling

--- Controllers/ReportsController.cs
+++ Controllers/ReportsController.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace SportsManagementSystem.Controllers
 {
     public class ReportsController : Controller
     {
+        [Authorize(Roles = "EventManager")]
         public IActionResult Index()
         {
             return View();
